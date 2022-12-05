ae9267c133e8a33a05724dbfc378c05e3e5b4f63
joakimhveem <joakim.hveem@riksrevisionen.se>
2022-12-05 15:09:53 +0100

    more work

--- train_sentenceKB_meta.py
+++ train_sentenceKB_meta.py
@@ -17,18 +17,28 @@ logger.addHandler(sh)
 
 PATH_TO_DATA = Path('data')
 INDATA = 'meta_prop_sou.pkl'
+N_TOPICS=32 #Number of topics
 
 df=pd.read_pickle(PATH_TO_DATA / INDATA)
-df=df.reset_index()
+df=df.set_index('dok_id') # Set correct index
+
+logger.info(f'Loaded {df.shape[0]} documents')
+
 
 # Load model
 sentence_model = SentenceTransformer('KBLab/sentence-bert-swedish-cased').encode
 # Load tokenizer
 
 tokenizer = AutoTokenizer.from_pretrained('KBLab/sentence-bert-swedish-cased').tokenize
-model = Top2Vec(df['titel'].values, split_documents=False, embedding_model=sentence_model, use_embedding_model_tokenizer=True, tokenizer=tokenizer)
+model = Top2Vec(df['titel'].values, document_ids=df.index.to_list(), split_documents=False, embedding_model=sentence_model, use_embedding_model_tokenizer=True, tokenizer=tokenizer, workers=8)
 
 model_path=r"models\Modell_sbertKB_titel.mod"
+
+
+# Reduce number of topics to N_TOPICS
+logger.info('Reduce number of topics')
+model.hierarchical_topic_reduction(N_TOPICS)
+
 model.save(model_path)
 
 logger.info('Done!')
